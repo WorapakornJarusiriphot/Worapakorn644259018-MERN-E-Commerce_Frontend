18ff623252b0c9d10c0c7431395f7d51ec99168d
worapakorn <pupupuwora@gmail.com>
2024-03-03 03:06:20 +0700

    npm i @tanstack/react-query || npm i axios || npm i slick-carousel || npm i sweetalert2 || Update the ProductList.jsx file by connecting to the database in MongoDB.

--- src/pages/shop/ProductList.jsx
+++ src/pages/shop/ProductList.jsx
@@ -13,7 +13,7 @@ const ProductList = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/product.json");
+        const response = await fetch("http://localhost:5000/products");
         const data = await response.json();
         setProducts(data);
         setFilteredItems(data);
